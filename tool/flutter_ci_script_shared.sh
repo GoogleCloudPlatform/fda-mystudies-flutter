1665c69f5a1030ccf4fbe94d97381fec04715cc0
Chintan Ghate <chintanghate@google.com>
2021-10-08 21:51:18 +0530

    Ignore formatter on generated code in fda_mystudies_spec library

--- tool/flutter_ci_script_shared.sh
+++ tool/flutter_ci_script_shared.sh
@@ -15,7 +15,7 @@ function ci_projects () {
         dart analyze
 
         # Run the formatter on all the dart files to make sure everything's linted.
-        dart format --output none --set-exit-if-changed .
+        find . -name "*.dart" ! -path './package/fda_mystudies_spec/*' | xargs dart format --set-exit-if-changed
 
         if [ "${PROJECT_NAME}" == "package/fda_mystudies_spec" ]
         then
