5a692c2f3cf9151087453ca5b7df8a8d5d4ff28d
Chintan Ghate <chintanghate@google.com>
2021-11-09 08:23:51 +0530

    Implement ParticipantUserDatastoreService  methods

--- package/fda_mystudies_http_client/lib/service/participant_user_datastore_service/participant_user_datastore_service.dart
+++ package/fda_mystudies_http_client/lib/service/participant_user_datastore_service/participant_user_datastore_service.dart
@@ -1,17 +1,24 @@
+import 'package:fda_mystudies_spec/participant_user_datastore_service/get_user_profile.pb.dart';
+
 abstract class ParticipantUserDatastoreService {
-  Future<Object> register();
+  Future<Object> register(String emailId, String password);
 
-  Future<Object> verifyEmail();
+  Future<Object> verifyEmail(
+      String emailId, String userId, String verificationCode);
 
-  Future<Object> resendConfirmation();
+  Future<Object> resendConfirmation(String userId, String emailId);
 
-  Future<Object> getUserProfile();
+  Future<Object> getUserProfile(String userId, String authToken);
 
-  Future<Object> updateUserProfile();
+  Future<Object> updateUserProfile(String userId, String authToken,
+      GetUserProfileResponse_UserProfile userProfileSettings);
 
-  Future<Object> feedback();
+  Future<Object> feedback(
+      String userId, String authToken, String subject, String feedbackBody);
 
-  Future<Object> contactUs();
+  Future<Object> contactUs(String userId, String authToken, String subject,
+      String feedbackBody, String email, String firstName);
 
-  Future<Object> deactivate();
+  Future<Object> deactivate(
+      String userId, String authToken, String studyId, String participantId);
 }
