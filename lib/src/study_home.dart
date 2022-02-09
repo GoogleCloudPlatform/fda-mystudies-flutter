303fb91973cc47f23e4e740ed8435bdf2d7262ab
Chintan Ghate <chintanghate@google.com>
2022-02-09 04:39:39 +0000

    Implement Drawer skeleton for CupertinoPageScaffold

--- lib/src/study_home.dart
+++ lib/src/study_home.dart
@@ -15,6 +15,8 @@ class StudyHome extends StatefulWidget {
 
 class _StudyHomeState extends State<StudyHome> {
   var _currentTab = 0;
+  var _showDrawer = false;
+  double movement = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -23,37 +25,96 @@ class _StudyHomeState extends State<StudyHome> {
     const resourcesTitle = 'Resources';
     var tabs = [activitiesTitle, dashboardTitle, resourcesTitle];
     var views = const [Activities(), Dashboard(), Resources()];
+    bool isDarkModeEnabled =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
 
     if (Theme.of(context).platform == TargetPlatform.iOS) {
-      return CupertinoPageScaffold(
-          navigationBar:
-              CupertinoNavigationBar(middle: Text(tabs[_currentTab])),
-          child: CupertinoTabScaffold(
-            tabBar: CupertinoTabBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.waveform, size: 24),
-                  label: activitiesTitle,
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.graph_square, size: 24),
-                  label: dashboardTitle,
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.book, size: 24),
-                  label: resourcesTitle,
-                ),
-              ],
-              onTap: (value) => setState(() {
-                _currentTab = value;
-              }),
-            ),
-            tabBuilder: (BuildContext context, int index) {
-              return views[index];
-            },
-          ));
+      return Stack(children: [
+        CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+                leading: CupertinoButton(
+                    padding: EdgeInsets.zero,
+                    alignment: Alignment.center,
+                    child: Icon(CupertinoIcons.bars,
+                        size: 28,
+                        color: isDarkModeEnabled
+                            ? CupertinoColors.extraLightBackgroundGray
+                            : CupertinoColors.darkBackgroundGray),
+                    onPressed: () {
+                      setState(() {
+                        _showDrawer = !_showDrawer;
+                      });
+                    }),
+                middle: Text(tabs[_currentTab])),
+            child: CupertinoTabScaffold(
+              tabBar: CupertinoTabBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(CupertinoIcons.waveform, size: 24),
+                    label: activitiesTitle,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(CupertinoIcons.graph_square, size: 24),
+                    label: dashboardTitle,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(CupertinoIcons.book, size: 24),
+                    label: resourcesTitle,
+                  ),
+                ],
+                onTap: (value) => setState(() {
+                  _currentTab = value;
+                }),
+              ),
+              tabBuilder: (BuildContext context, int index) {
+                return views[index];
+              },
+            )),
+        Visibility(
+            visible: _showDrawer,
+            child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _showDrawer = !_showDrawer;
+                  });
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  decoration:
+                      BoxDecoration(color: Colors.black.withOpacity(0.5)),
+                ))),
+        AnimatedPositioned(
+          left: _showDrawer
+              ? 0 + movement
+              : -(MediaQuery.of(context).size.width - 100),
+          duration: const Duration(milliseconds: 400),
+          child: GestureDetector(
+              onHorizontalDragUpdate: (details) {
+                setState(() {
+                  if (movement + details.delta.dx <= 0) {
+                    movement += details.delta.dx;
+                    if (movement < -100) {
+                      _showDrawer = false;
+                    }
+                  }
+                });
+              },
+              onHorizontalDragEnd: (details) {
+                setState(() {
+                  movement = 0;
+                });
+              },
+              child: Container(
+                color: CupertinoTheme.of(context).barBackgroundColor,
+                width: MediaQuery.of(context).size.width - 100,
+                height: MediaQuery.of(context).size.height,
+              )),
+        ),
+      ]);
     }
     return Scaffold(
+      drawer: const Drawer(),
       appBar: AppBar(title: Text(tabs[_currentTab])),
       body: IndexedStack(
         children: views,
