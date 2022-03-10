9ca3d0f205ec2a2868ac36dabe2d9534f38f89b9
Xiao Yu <xiao@xster.net>
2022-03-10 14:01:11 -0800

    make a fake commit to be able to review

--- lib/config/platform_config.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:fda_mystudies_activity_ui_kit/fda_mystudies_activity_ui_kit.dart';
-
-class PlatformConfig extends Config {
-  @override
-  String get operatingSystem => Platform.isIOS ? 'ios' : 'android';
-}
--- lib/src/dashboard_module/trends/recorded_value.dart
+++
@@ -1,6 +0,0 @@
-class RecordedValue {
-  final DateTime recordedAt;
-  final String value;
-
-  RecordedValue(this.recordedAt, this.value);
-}
