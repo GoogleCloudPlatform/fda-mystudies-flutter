eb5db63e2ffcdf092c32423102f0118e8f8df08b
Chintan Ghate <chintanghate@google.com>
2022-04-25 12:32:43 +0100

    Add screen diff test

---
+++ test/activities_module/activities_test.dart
@@ -0,0 +1,25 @@
+import 'package:fda_mystudies/fda_mystudies_app.dart';
+import 'package:fda_mystudies/src/study_home.dart';
+import 'package:fda_mystudies_http_client/fda_mystudies_http_client.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+void main() {
+  setUpAll(() {
+    configureDependencies(DemoConfig());
+  });
+
+  testGoldens('Show Activities page in android', (WidgetTester tester) async {
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: [Device.iphone11, Device.phone])
+      ..addScenario(widget: const StudyHome());
+
+    await loadAppFonts();
+    await tester.pumpDeviceBuilder(builder,
+        wrapper: materialAppWrapper(theme: FDAMyStudiesApp.androidLightTheme));
+    await screenMatchesGolden(tester, 'activity_home_light');
+    await tester.pumpDeviceBuilder(builder,
+        wrapper: materialAppWrapper(theme: FDAMyStudiesApp.androidDarkTheme));
+    await screenMatchesGolden(tester, 'activity_home_dark');
+  });
+}
