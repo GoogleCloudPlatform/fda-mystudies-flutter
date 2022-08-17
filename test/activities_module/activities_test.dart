940a0bc2691356e8b6b282a1cec29d8060f64f3a
Chintan Ghate <chintanghate@google.com>
2022-08-17 15:42:51 +0100

    Support internationalization changes in screen-diff tests

--- test/activities_module/activities_test.dart
+++ test/activities_module/activities_test.dart
@@ -1,6 +1,7 @@
 import 'package:fda_mystudies/fda_mystudies_app.dart';
 import 'package:fda_mystudies/src/study_home.dart';
 import 'package:fda_mystudies_http_client/fda_mystudies_http_client.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
@@ -13,10 +14,16 @@ void main() {
     final builder = DeviceBuilder()..addScenario(widget: const StudyHome());
 
     await tester.pumpDeviceBuilder(builder,
-        wrapper: materialAppWrapper(theme: FDAMyStudiesApp.androidLightTheme));
+        wrapper: materialAppWrapper(
+            theme: FDAMyStudiesApp.androidLightTheme,
+            localizations: AppLocalizations.localizationsDelegates,
+            localeOverrides: AppLocalizations.supportedLocales));
     await screenMatchesGolden(tester, 'activity_home_light');
     await tester.pumpDeviceBuilder(builder,
-        wrapper: materialAppWrapper(theme: FDAMyStudiesApp.androidDarkTheme));
+        wrapper: materialAppWrapper(
+            theme: FDAMyStudiesApp.androidDarkTheme,
+            localizations: AppLocalizations.localizationsDelegates,
+            localeOverrides: AppLocalizations.supportedLocales));
     await screenMatchesGolden(tester, 'activity_home_dark');
   });
 }
