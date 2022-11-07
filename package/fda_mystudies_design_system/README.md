9af97ab2d51f82913f393faa2a3d4a4724467ce4
Chintan Ghate <chintanghate@google.com>
2022-11-07 14:30:20 +0000

    Create FDAMyStudiesDesignSystem package to create a unified design system throughout the app and Activity UI kit

---
+++ package/fda_mystudies_design_system/README.md
@@ -0,0 +1,3 @@
+# FDA MyStudies Design System
+
+The package contains all the common UI elements used throughout mail app UI & Activity UI Kit.
---
+++ package/fda_mystudies_design_system/test/fda_mystudies_design_system_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fda_mystudies_design_system/fda_mystudies_design_system.dart';
+
+void main() {}
