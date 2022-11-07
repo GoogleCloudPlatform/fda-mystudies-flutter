78c6f0ad8cda9aa4530436685ec7b6e4ec3c577b
Chintan Ghate <chintanghate@google.com>
2022-11-07 14:57:48 +0000

    Add sample test to fix failing github workflows

--- package/fda_mystudies_design_system/test/fda_mystudies_design_system_test.dart
+++ package/fda_mystudies_design_system/test/fda_mystudies_design_system_test.dart
@@ -2,4 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:fda_mystudies_design_system/fda_mystudies_design_system.dart';
 
-void main() {}
+void main() {
+  test("sample test", () {
+    expect(1 + 2, 3);
+  });
+}
