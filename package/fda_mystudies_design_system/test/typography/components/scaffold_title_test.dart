f0be748564369fc44aefb3bd8166951e41d20f32
Chintan Ghate <chintanghate@google.com>
2022-11-11 04:54:07 +0000

    Move ScaffoldTitle to components directory

--- package/fda_mystudies_design_system/test/typography/components/scaffold_title_test.dart
+++ package/fda_mystudies_design_system/test/typography/components/scaffold_title_test.dart
@@ -1,8 +1,8 @@
-import 'package:fda_mystudies_design_system/typography/scaffold_title.dart';
+import 'package:fda_mystudies_design_system/typography/components/scaffold_title.dart';
 import 'package:flutter/material.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
-import '../test_util.dart';
+import '../../test_util.dart';
 
 void main() {
   testGoldens('ScaffoldTitle should be displayed correctly', (tester) async {
