182ff49affcfa2a3c76729ed821fa842898ce67a
Chintan Ghate <chintanghate@google.com>
2023-02-17 14:10:18 +0000

    Allow Biometric local auth for Android

--- android/app/src/main/kotlin/com/google/fda_mystudies/MainActivity.kt
+++ android/app/src/main/kotlin/com/google/fda_mystudies/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.google.fda_mystudies
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
