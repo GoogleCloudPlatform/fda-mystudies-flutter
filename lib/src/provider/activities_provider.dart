8906d9af46869a43b22a3b5c519d824e93aae93f
Chintan Ghate <chintanghate@google.com>
2023-05-15 19:33:40 +0100

    Fix activity order depending on defaults, frequency and status.
    GitOrigin-RevId: cc67e2373e963a411cd7d8950d533cb2aee585fe

--- lib/src/provider/activities_provider.dart
+++ lib/src/provider/activities_provider.dart
@@ -2,6 +2,8 @@ import 'package:fda_mystudies_design_system/block/activity_tile_block.dart';
 import 'package:fda_mystudies_spec/response_datastore_service/get_activity_state.pb.dart';
 import 'package:flutter/material.dart';
 
+import '../../config/app_defaults.dart';
+
 class ActivitiesProvider extends ChangeNotifier {
   List<ActivityBundle>? _activityBundleList;
 
@@ -14,7 +16,28 @@ class ActivitiesProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  List<ActivityBundle> get activityBundleList => _activityBundleList ?? [];
+  List<ActivityBundle> get activityBundleList {
+    var list = _activityBundleList ?? [];
+    list.sort((a, b) {
+      // Resume, Start, Upcoming, Missed, Completed, Expired
+      final priority1 = ActivityStatusExtension.valueFrom(a.state.activityState)
+        .order
+        .compareTo(
+            ActivityStatusExtension.valueFrom(b.state.activityState).order);
+      
+      // One-Time, Daily, Weekly, Monthly, Custom
+      final priority2 = a.frequency.index.compareTo(b.frequency.index);
+
+      // Default activity order
+      final priority3 = AppDefaults.order
+        .indexOf(a.activityId)
+        .compareTo(AppDefaults.order.indexOf(b.activityId));
+      
+      final result = (priority1 * 100) + (priority2 * 10) + priority3;
+      return result == 0 ? 0 : (result > 0 ? 1 : -1);
+    });
+    return list;
+  }
 }
 
 class ActivityBundle {
